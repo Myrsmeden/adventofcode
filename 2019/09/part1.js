00a6b3a2562f39d3d1e44fc8e27c679b43aa30fe
Johan Myrsmeden <johan@myrsmeden.se>
2019-12-09 22:21:06 +0100

    Adding solutions for day 9

---
+++ 2019/09/part1.js
@@ -0,0 +1,170 @@
+const fs = require('fs')
+
+const add = (a, b) => a + b
+
+const multi = (a, b) => a * b
+
+const getDigitsAndCode = instruction => {
+  if (('' + instruction).length === 1) {
+    return [[0, 0, 0, 0, instruction], instruction]
+  }
+
+  const digits = ('' + instruction).split('')
+  const code = parseInt(
+    `${digits[digits.length - 2]}${digits[digits.length - 1]}`
+  )
+
+  return [digits, code]
+}
+
+const parameterMode = (digits, paramNumber) => {
+  try {
+    return parseInt(digits[digits.length - paramNumber - 2])
+  } catch (e) {
+    return 0
+  }
+}
+
+class Amplifier {
+  constructor(phaseSetting, codes) {
+    this.phaseSetting = phaseSetting
+    this.codes = codes.reduce((acc, code, i) => {
+      acc[i] = code
+      return acc
+    }, {})
+    this.halted = false
+    this.i = 0
+    this.relativeBase = 0
+  }
+
+  getParam(digits, codes, i, paramNumber) {
+    const mode = parameterMode(digits, paramNumber)
+    switch (mode) {
+      case 2:
+        return codes[this.relativeBase + codes[i + paramNumber] || 0]
+      case 1:
+        return codes[i + paramNumber] || 0
+      case 0:
+      default:
+        return codes[codes[i + paramNumber]] || 0
+    }
+  }
+
+  getParamPositional(digits, codes, i, paramNumber) {
+    const mode = parameterMode(digits, paramNumber)
+    switch (mode) {
+      case 2:
+        return this.relativeBase + codes[i + paramNumber] || 0
+      case 1:
+        return i + paramNumber
+      case 0:
+      default:
+        return codes[i + paramNumber]
+    }
+  }
+
+  run(input) {
+    let output = -1
+    let n = 0
+    while (this.i < Object.keys(this.codes).length) {
+      n += 1
+      const instruction = this.codes[this.i]
+      const [digits, code] = getDigitsAndCode(instruction)
+      if (code === 99) {
+        this.halted = true
+        return output
+      }
+
+      if (code === 1) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+
+        const param2 = this.getParam(digits, this.codes, this.i, 2)
+
+        const result = add(param1, param2)
+        this.codes[
+          this.getParamPositional(digits, this.codes, this.i, 3)
+        ] = result
+        this.i += 4
+      }
+
+      if (code === 2) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        const param2 = this.getParam(digits, this.codes, this.i, 2)
+        const result = multi(param1, param2)
+        this.codes[
+          this.getParamPositional(digits, this.codes, this.i, 3)
+        ] = result
+        this.i += 4
+      }
+
+      if (code === 3) {
+        this.codes[this.getParamPositional(digits, this.codes, this.i, 1)] =
+          this.i === 0 ? phaseSetting : input
+        this.i += 2
+      }
+
+      if (code === 4) {
+        output = this.getParamPositional(digits, this.codes, this.i, 1)
+        console.log(this.codes[output])
+        this.i += 2
+      }
+
+      if (code === 5) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        if (param1 != 0) {
+          this.i = this.getParam(digits, this.codes, this.i, 2)
+        } else {
+          this.i += 3
+        }
+      }
+
+      if (code === 6) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        if (param1 === 0) {
+          this.i = this.getParam(digits, this.codes, this.i, 2)
+        } else {
+          this.i += 3
+        }
+      }
+
+      if (code === 7) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        const param2 = this.getParam(digits, this.codes, this.i, 2)
+        const param3 = this.getParamPositional(digits, this.codes, this.i, 3)
+        if (param1 < param2) {
+          this.codes[param3] = 1
+        } else {
+          this.codes[param3] = 0
+        }
+        this.i += 4
+      }
+
+      if (code === 8) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        const param2 = this.getParam(digits, this.codes, this.i, 2)
+        const param3 = this.getParamPositional(digits, this.codes, this.i, 3)
+        if (param1 === param2) {
+          this.codes[param3] = 1
+        } else {
+          this.codes[param3] = 0
+        }
+
+        this.i += 4
+      }
+
+      if (code === 9) {
+        const param1 = this.getParam(digits, this.codes, this.i, 1)
+        this.relativeBase += param1
+        this.i += 2
+      }
+    }
+  }
+}
+
+fs.readFile('input.txt', 'utf8', (_, data) => {
+  const codes = data.split(',').map(x => parseInt(x))
+
+  const amp = new Amplifier(1, codes.slice())
+  amp.run(1)
+  return
+})
