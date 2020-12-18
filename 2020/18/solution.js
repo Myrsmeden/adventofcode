426484250c7c0e6d3c4ab86499d9e0d834bc328e
Johan Myrsmeden <johan@myrsmeden.se>
2020-12-18 22:18:33 +0100

    Solutions to day 18

---
+++ 2020/18/solution.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), {
+  encoding: 'utf8',
+  flag: 'r',
+});
+
+const expressions = input.split('\n');
+const catchParenthesis = /\([^\(\)]+\)/g;
+
+const evaluate = (string) => {
+  const operands = string.split(' ');
+  let value = parseInt(operands[0]);
+
+  for (let i = 2; i < operands.length; i++) {
+    if (operands[i - 1] === '+') {
+      value += parseInt(operands[i]);
+    }
+
+    if (operands[i - 1] === '*') {
+      value *= parseInt(operands[i]);
+    }
+  }
+  return value;
+};
+
+const evaluateExpression = (expression) => {
+  let parenthesis;
+  while (true) {
+    parenthesis = expression.match(catchParenthesis);
+    if (parenthesis) {
+      parenthesis.forEach((part) => {
+        const value = evaluate(part.slice(1, -1));
+        expression = expression.replace(part, value.toString());
+      });
+    } else {
+      break;
+    }
+  }
+  return evaluate(expression);
+};
+
+const part1 = () => {
+  const results = expressions.map((expression) => {
+    return evaluateExpression(expression);
+  });
+  return results.reduce((acc, value) => acc + value, 0);
+};
+
+console.log(part1());
